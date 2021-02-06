de775f8f6a06d1f2abbd1555475c61c20ce67d28
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2021-02-06 19:16:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,6 +25,14 @@ class Dog
 
     DB[:conn].execute(sql)
   end
-  
+
+  def self.new_from_db(row)
+    attributes_hash = {
+      :id => row[0],
+      :name => row[1],
+      :breed => row[2]
+    }
+    self.new(attributes_hash)
+  end
 
 end
